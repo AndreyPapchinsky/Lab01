b50193b8ef7159abadcd0cc12e4ae13c051647df
AndreyPapchinsky <Ha.xJle6ywek@yandex.ru>
2021-11-20 18:17:49 +0300

    Добавьте файлы проекта.

---
+++ Lab01/Lab01.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ExerciseNumber5
+{
+    class Lab01
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                double p2;
+                Console.WriteLine("Введите значение периметра равностороннего треугольника: ");
+                p2 = double.Parse(Console.ReadLine());
+
+                double a = p2 / 3;
+                Console.WriteLine("Сторона треугольника: ");
+                Console.WriteLine("{0:F2}", a);
+
+                double p = p2 / 2;
+                double S = Math.Sqrt(p * (p - a) * (p - a) * (p - a));
+                Console.WriteLine("Площадь равностороннего треугольника: ");
+                Console.WriteLine("{0:F2}", S);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка: " + ex.Message);
+            }
+        }
+    }
+}
